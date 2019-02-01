1d90144c47eeb3ce9806bd76e2f1e72ce9bb9dd5
Nick Mckloski <mckloski.nick@gmail.com>
2019-02-01 12:49:08 -0500

    created first model and migration

---
+++ Api/Blog.Data/BlogContext.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Data
+{
+    public class BlogContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+    }
+}
---
+++ Api/Blog.Domain/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Domain.Models
+{
+    public class Post : Entity
+    {
+
+        public Post(string title, string content)
+        {
+            Title = title;
+            Content = content;
+        }
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
